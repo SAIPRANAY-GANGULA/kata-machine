3dcd59c5e11ce1b04debdf7053fad93dcc9d5918
PranayBackbase <saig@backbase.com>
2023-04-04 23:48:21 +0530

    binary-search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,17 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let low = 0;
+    let high = haystack.length;
 
+    do {
+        const middle = Math.floor(low + (high - low) / 2);
+        if (haystack[middle] === needle) {
+            return true;
+        } else if (haystack[middle] > needle) {
+            high = middle;
+        } else {
+            low = middle + 1;
+        }
+    } while (low < high);
+
+    return false;
 }
